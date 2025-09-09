b619254937bc9ac1fb0d115c8db212458fc13730
kareemtarekK <engkareemtarek555@gmail.com>
2025-09-09 16:41:03 +0300

    rename product model

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,5 @@
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
-const Product = require("./../models/product.js");
 exports.createProduct = catchAsync(async (req, res, next) => {
   const newProduct = await Product.create(req.body);
   res.status(201).json({
