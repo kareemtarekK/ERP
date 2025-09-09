c205e3e88d965180be9990ca5fd8f2a27de9691a
anawartarekA <anwartarek719@gmail.com>
2025-09-09 14:33:57 +0100

    fix product model

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
-const Product = require("./../models/product.js");
+const Product = require("../models/productModel.js");
 exports.createProduct = catchAsync(async (req, res, next) => {
   const newProduct = await Product.create(req.body);
   res.status(201).json({
