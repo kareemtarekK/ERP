eacee483c598ffa430f38f402f06e0384a959ad7
anawartarekA <anwartarek719@gmail.com>
2025-09-24 13:47:40 +0100

    update & delete purchase order

--- controllers/purchaseOrderController.js
+++ controllers/purchaseOrderController.js
@@ -1,5 +1,7 @@
 const PurchaseOrder = require("./../models/purchaseOrderModel");
 const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+// create purchase order
 exports.createPurchaseOrder = catchAsync(async (req, res, next) => {
   const purchaseOrder = await PurchaseOrder.create(req.body);
   res.status(201).json({
@@ -7,5 +9,38 @@ exports.createPurchaseOrder = catchAsync(async (req, res, next) => {
     data: {
       purchaseOrder,
     },
+  });
+});
+// update pruchase order
+exports.updatePurchaseOrder = catchAsync(async (req, res, next) => {
+  const { purchaseOrderId } = req.params;
+  if (!purchaseOrderId)
+    return next(new AppError("Purchase order id required", 500));
+  const updatePurchaseOrder = await PurchaseOrder.findByIdAndUpdate(
+    purchaseOrderId,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  if (!updatePurchaseOrder)
+    return next(new AppError("No purchase order found with that ID", 404));
+  res.status(200).json({
+    status: "success",
+    data: {
+      updatePurchaseOrder,
+    },
+  });
+});
+// delete purchase order
+exports.deletePurchaseOrder = catchAsync(async (req, res, next) => {
+  const { purchaseOrderId } = req.params;
+  if (!purchaseOrderId)
+    return next(new AppError("Purchase order id required", 500));
+  await PurchaseOrder.findByIdAndDelete(purchaseOrderId);
+  res.status(204).json({
+    status: "success",
+    data: null,
   });
 });
--- routers/purchaseOrderRouter.js
+++ routers/purchaseOrderRouter.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const {
   createPurchaseOrder,
+  updatePurchaseOrder,
+  deletePurchaseOrder,
 } = require("../controllers/purchaseOrderController");
 const router = express.Router();
 router.post("/", createPurchaseOrder);
+router
+  .route("/:purchaseOrderId")
+  .patch(updatePurchaseOrder)
+  .delete(deletePurchaseOrder);
 module.exports = router;
