e994469e21d31471e2e7cc2ce8a6800faa0693eb
anawartarekA <anwartarek719@gmail.com>
2025-09-28 14:01:03 +0100

    stock v3

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,6 +1,6 @@
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
-
+const Stock = require("./../models/stockModel");
 // add product to inventory
 exports.addStockToInventory = catchAsync(async (req, res, next) => {
   const stock = await Stock.create(req.body);
